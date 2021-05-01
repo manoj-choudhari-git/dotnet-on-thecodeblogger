0e34589b0e8624c09a4537cb891a4fa9ef3da679
Manoj Choudhari <manoj_choudhari@outlook.com>
2021-05-01 21:58:30 +0200

    Demo for using appsettings.json with console app

---
+++ ConsoleAppConfigDemo/ConsoleAppConfigDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleAppConfigDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = CreateDefaultBuilder().Build();
+            using IServiceScope serviceScope = host.Services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+            var workerInstance = provider.GetRequiredService<Worker>();
+            workerInstance.DoWork();
+            host.Run();
+        }
+
+        static IHostBuilder CreateDefaultBuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration(app =>
+                {
+                    app.AddJsonFile("appsettings.json");
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<Worker>();
+                });
+        }
+    }
+}
---
+++ ConsoleAppConfigDemo/ConsoleAppConfigDemo/Worker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleAppConfigDemo
+{
+    internal class Worker
+    {
+        private readonly IConfiguration configuration;
+
+        public Worker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void DoWork()
+        {
+            var keyValuePairs = configuration.AsEnumerable().ToList();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("==============================================");
+            Console.WriteLine("Configurations...");
+            Console.WriteLine("==============================================");
+            foreach (var pair in keyValuePairs)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value}");
+            }
+            Console.WriteLine("==============================================");
+            Console.ResetColor();
+        }
+    }
+}
