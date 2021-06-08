2412d508b0e2b27a5a404beb0ce10c4ac8687185
Manoj Choudhari <manoj_choudhari@outlook.com>
2021-06-08 23:30:40 +0200

    Distributed SQL Cache Sample

---
+++ WebApiDistributedSqlCache/WebApiDistributedSqlCache/Controllers/WeatherForecastController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+
+namespace WebApiDistributedSqlCache.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IDistributedCache _distributedCache;
+        private readonly string WeatherForecastKey = "WeatherForecast";
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IDistributedCache distributedCache)
+        {
+            _logger = logger;
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            // Find cached item
+            byte[] objectFromCache = await _distributedCache.GetAsync(WeatherForecastKey);
+
+            if (objectFromCache != null)
+            {
+                // Deserialize it
+                var jsonToDeserialize = System.Text.Encoding.UTF8.GetString(objectFromCache);
+                var cachedResult = JsonSerializer.Deserialize<IEnumerable<WeatherForecast>>(jsonToDeserialize);
+                if (cachedResult != null)
+                {
+                    // If found, then return it
+                    return cachedResult;
+                }
+            }
+
+            // If not found, then recalculate response
+            var result = GenerateResponse();
+
+            // Serialize the response
+            byte[] objectToCache = JsonSerializer.SerializeToUtf8Bytes(result);
+            var cacheEntryOptions = new DistributedCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromSeconds(10))
+                .SetAbsoluteExpiration(TimeSpan.FromSeconds(30));
+
+            // Cache it
+            await _distributedCache.SetAsync(WeatherForecastKey, objectToCache, cacheEntryOptions);
+
+            return result;
+        }
+
+        private static IEnumerable<WeatherForecast> GenerateResponse()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
