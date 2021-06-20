c9e8cd2ed186d7dcf32b2d7acf2f4fd4a6564a69
Manoj Choudhari <manoj_choudhari@outlook.com>
2021-06-20 15:36:36 +0200

    EF Core Sample Added

---
+++ EFCoreConcepts/University.Data.EF.Models/DesignTimeUniversityContextFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.Configuration;
+
+namespace University.Data.EF.Models
+{
+
+    public class DesignTimeUniversityContextFactory : IDesignTimeDbContextFactory<UniversityContext>
+    {
+        public UniversityContext CreateDbContext(string[] args)
+        {
+            //var configuration = new ConfigurationBuilder()
+            //     .SetBasePath(Directory.GetCurrentDirectory())
+            //     .AddJsonFile("appsettings.json")
+            //     .Build();
+
+            var dbContextBuilder = new DbContextOptionsBuilder<UniversityContext>();
+
+            var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=University; Integrated Security=True;";
+
+            dbContextBuilder.UseSqlServer(connectionString);
+
+            return new UniversityContext(dbContextBuilder.Options);
+        }
+    }
+}
---
+++ EFCoreConcepts/University.Data.EF.Models/Migrations/20210620130352_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace University.Data.EF.Models.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false, defaultValue: "Not Mentioned"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: true, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students",
+                schema: "dbo");
+        }
+    }
+}
