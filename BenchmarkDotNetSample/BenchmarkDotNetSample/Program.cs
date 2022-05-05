12eb7fcbad283d54c391edc440cc99c4ef549921
Manoj Choudhari <manoj_choudhari@outlook.com>
2022-05-05 09:06:22 +0200

    BenchmarkDotNet sample
    BenchmarkDotNet sample

---
+++ BenchmarkDotNetSample/BenchmarkDotNetSample/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using BenchmarkDotNetSample;
+using System.Diagnostics;
+
+Stopwatch sw = Stopwatch.StartNew();
+
+var summary = BenchmarkRunner.Run<StringCompareUtil>();
+
+sw.Stop();
+Console.WriteLine("=================================================================");
+Console.WriteLine($"Total Elapsed Milliseconds: {sw.ElapsedMilliseconds}");
+Console.WriteLine("=================================================================");
+
+
+
+//sw.Start();
+//var summaries = BenchmarkRunner.Run<MyBenchmarks>();
+
+
+//Console.WriteLine("=================================================================");
+//Console.WriteLine($"Total Elapsed Milliseconds: {sw.ElapsedMilliseconds}");
+//Console.WriteLine("=================================================================");
+
---
+++ BenchmarkDotNetSample/BenchmarkDotNetSample/StringCompareUtil.cs
@@ -0,0 +1,54 @@
+﻿using BenchmarkDotNet.Attributes;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkDotNetSample
+{
+    public class StringCompareUtil
+    {
+        [ParamsSource(nameof(ValuesForFirst))]
+        public string First { get; set; }
+
+        [ParamsSource(nameof(ValuesForSecond))]
+        public string Second { get; set; }
+
+        // public methods
+        public IEnumerable<string> ValuesForFirst => new string[] { "some-random-string", "another-string" };
+        public IEnumerable<string> ValuesForSecond => new string[] { "some-random-string", "non-matching" };
+
+        [Benchmark]
+        public bool EqualUsingCompare()
+        {
+            if (First == null || Second == null)
+            {
+                return false;
+            }
+
+            return string.Compare(First, Second, StringComparison.OrdinalIgnoreCase) == 0;
+        }
+
+        [Benchmark]
+        public bool EqualUsingOperator()
+        {
+            if (First == null || Second == null)
+            {
+                return false;
+            }
+            return First == Second;
+        }
+
+        [Benchmark]
+        public bool EqualUsingEquals()
+        {
+            if (First == null || Second == null)
+            {
+                return false;
+            }
+            return First.Equals(Second);
+        }
+    }
+}
